0a88fb17e4f969a518723d69ec7fc9c290839ef9
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2022-04-14 13:29:40 -0500

    New era

---
+++ AntiguosCPP/HolaMundo.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<< "Ingrese un numero cualquiera \n";
+    cin >> n;
+    cout<< "Holaaaa Ya Puedes programar :) \n";
+
+
+    return 0;
+}
