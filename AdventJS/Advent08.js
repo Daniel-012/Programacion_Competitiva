255b464996a0f33bea6bcffe662bdcf5489cdc57
Daniel1209 <danielgarciamendoza.204@gmail.com>
2022-12-10 00:04:14 -0600

    Advent08.js

--- AdventJS/Advent08.js
+++ AdventJS/Advent08.js
@@ -1,13 +1,35 @@
-function checkPart(part) {
-  for (let i = 0; i < part.length; i += 1) {
-    const sub = `${part.substring(0, i) + part.substring(i + 1, part.length)}`;
-    console.log(sub);
-    if (sub === sub.split("").reverse().join()) {
-      return true;
-    }
-  }
+// 80 Puntos
+// function checkPart(part) {
+//   for (let i = 0; i < part.length; i += 1) {
+//     const sub = `${part.substring(0, i) + part.substring(i + 1, part.length)}`;
+//     // console.log();
+//     if (sub === sub.split("").reverse().join("")) {
+//       return true;
+//     }
+//   }
 
-  return false;
+//   return false;
+// }
+
+function checkPart(part) {
+  part = part.split("");
+  // console.log(part);
+  return part.some((a, i) => {
+    let sub = part.filter((_, y) => y != i);
+    console.log(sub.join(), sub.reverse().join());
+
+    return sub.join() === sub.reverse().join();
+  });
+
 }
 
-console.log(checkPart("mimu"));
+// 200 Puntos
+// function checkPart(part) {
+//   return [...part].some((x, i,array) => {
+//     let w = array.filter((_, y) => y!= i);
+//     // console.log(w);
+//     return w.join() === w.reverse().join();
+//   })
+// }
+
+console.log(checkPart("lolol"));
